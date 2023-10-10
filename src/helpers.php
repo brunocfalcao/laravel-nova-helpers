560d95d49df1459f621cde9fd42143cc61551bec
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-10-10 22:44:41 +0200

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,23 @@
 <?php
+
+if (! function_exists('via_resource')) {
+    function via_resource(string|array $resources = [])
+    {
+        if (is_string($resources)) {
+            $resources = [$resources];
+        }
+
+        return
+            // HTTP method is GET.
+            request()->isMethod('GET') &&
+
+            // There is a query key 'viaResource'.
+            request()->has('viaResource') &&
+
+            // And that viaResource key is not blank.
+            ! blank(request()->input('viaResource')) &&
+
+            // ViaResource value is one of the argument array values.
+            in_array(request()->input('viaResource'), $resources);
+    }
+}
